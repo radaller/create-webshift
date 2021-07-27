a4e96a5ae584df2956386dcb194d1b9d1b5db49d
osvarychevskyi <a.ekspo@gmail.com>
2021-07-27 10:07:06 +0200

    - [Template] added Headers component to @app

--- react-emotion/src/App.js
+++ react-emotion/src/App.js
@@ -7,11 +7,27 @@ import WebpackLogo from './icon-webpack.svg';
 import ReactLogo from './icon-react.png';
 import EmotionLogo from './icon-emotion.png';
 import LoadableLogo from './icon-loadable.png';
+//import Favicon from "./favicon.ico";
+//import Logo192 from "./logo192.png";
 import theme from './theme';
 
 import Bar from './components/Bar';
 import Header from './components/Header';
 import Button from './components/Button';
+
+export const Headers = () => {
+    return (
+        <head>
+            <meta charSet="utf-8" />
+            {/*<link rel="icon" href={ Favicon } />*/}
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#000000" />
+            <meta name="description" content="Web site created using create-react-app" />
+            {/*<link rel="apple-touch-icon" href={ Logo192 } />*/}
+            <title>title</title>
+        </head>
+    );
+}
 
 export default () => {
 
